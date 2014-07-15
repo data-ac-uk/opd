6dac044acdf5950fa0c57251d33a35117edd74b8
Andy Milsted <a.j.milsted@soton.ac.uk>
2014-07-15 21:25:00 +0100

    added start of documentations made handy function to include templated basic pages

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,12 +9,22 @@ $f3->set('DEBUG',1);
 $f3->config('config.ini');
 
 $f3->route('GET /',
-	function($f3) {
+	function($f3) { basicPage( $f3, "Organisation Profile Documents", "welcome.html" ); }
+);
 
-		$f3->set('html_title', "Welcome to the OPD pages" );
-		$f3->set('content','welcome.html');
-		echo Template::instance()->render('page-template.html');
-	}
+$f3->route('GET /docs/feeds',
+	function($f3) { basicPage( $f3, "OPD Documentation: Feeds (RSS, iCal, Atom)", "docs-feeds.html" ); }
+);
+$f3->route('GET /docs/social',
+	function($f3) { basicPage( $f3, "OPD Documentation: Social Networks", "docs-social.html" ); }
 );
 
 $f3->run();
+exit;
+
+function basicPage( $f3, $title, $template )
+{
+	$f3->set('html_title', $title );
+	$f3->set('content', $template );
+	echo Template::instance()->render('page-template.html');
+}
