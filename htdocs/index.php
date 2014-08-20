01fed65ecc4dbc946801b63bfe0a198d4dc41562
Andrew Milsted <a.j.milsted@soton.ac.uk>
2014-08-20 15:28:21 +0100

    Page layout change

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,29 +9,37 @@ $f3->set('DEBUG',1);
 $f3->config('config.ini');
 
 $f3->route('GET /',
-	function($f3) { basicPage( $f3, "Organisation Profile Documents", "welcome.html" ); }
+	function($f3) { 
+		$opds = json_decode( file_get_contents( '../var/autoopds.json' ), true );
+		$f3->set('opds', $opds );
+		
+		basicPage( $f3, "Organisation Profile Documents", "welcome.html" ); 
+	}
 );
 
 $f3->route('GET /docs/feeds',
-	function($f3) { basicPage( $f3, "OPD Documentation: Feeds (RSS, iCal, Atom)", "docs-feeds.html" ); }
+	function($f3) { basicPage( $f3, "Documentation: Feeds (RSS, iCal, Atom)", "docs-feeds.html" ); }
 );
 $f3->route('GET /docs/social',
-	function($f3) { basicPage( $f3, "OPD Documentation: Social Networks", "docs-social.html" ); }
+	function($f3) { basicPage( $f3, "Documentation: Social Networks", "docs-social.html" ); }
 );
 $f3->route('GET /docs/auto',
-	function($f3) { basicPage( $f3, "OPD Documentation: Autodiscovery", "docs-auto.html" ); }
+	function($f3) { basicPage( $f3, "Documentation: Autodiscovery", "docs-auto.html" ); }
 );
 $f3->route('GET /docs/datasets',
-	function($f3) { basicPage( $f3, "OPD Documentation: Datasets", "docs-datasets.html" ); }
+	function($f3) { basicPage( $f3, "Documentation: Datasets", "docs-datasets.html" ); }
 );
 $f3->route('GET /workshops/getting_started',
-	function($f3) { basicPage( $f3, "Workshop: OPD Getting Started", "workshop-gettingstarted.html" ); }
+	function($f3) { basicPage( $f3, "Workshop: Getting Started", "workshop-gettingstarted.html" ); }
 );
 $f3->route('GET /docs/core',
-	function($f3) { basicPage( $f3, "OPD Documentation: Core", "docs-core.html" ); }
+	function($f3) { basicPage( $f3, "Documentation: Core", "docs-core.html" ); }
+);
+$f3->route('GET /docs/local',
+	function($f3) { basicPage( $f3, "Documentation: Locations", "docs-local.html" ); }
 );
 $f3->route('GET /docs/key-pages',
-	function($f3) { basicPage( $f3, "OPD Documentation: Key Pages", "docs-key-pages.html" ); }
+	function($f3) { basicPage( $f3, " Documentation: Key Pages", "docs-key-pages.html" ); }
 );
 
 $f3->run();
