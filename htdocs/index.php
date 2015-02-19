88b05a28f198850d94c2842f25608667e2674b8d
Andrew Milsted <a.j.milsted@soton.ac.uk>
2015-02-19 11:16:32 +0000

    Added an exporter to scripts

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,17 +5,10 @@ $f3=require('../f3/lib/base.php');
 if($_SERVER['HTTP_HOST']!='opd.data.ac.uk')
 	$f3->set('DEBUG',1);
 else{
-	$f3->set('ONERROR',
-	    function($f3) {
-	        // custom error handler code goes here
-	        // use this if you want to display errors in a
-	        // format consistent with your site's theme
-			echo "<h1>{$f3->get('ERROR.status')}</h1>\n";
-			if($f3->get('ERROR.code')==404)
-				echo $f3->get('ERROR.text');
-	    }
-	);
+	$f3->set('DEBUG',0);
 }
+
+
 $f3->set('AUTOLOAD',"app/");
 $f3->set('UI',"ui/");
 
@@ -65,6 +58,29 @@ $f3->route('GET /docs/key-pages',
 );
 
 
+$f3->route('GET /data/@name.@type',
+	function($f3) { 
+		$name = $f3->get( "PARAMS.name" );
+		$type =  $f3->get( "PARAMS.type" );
+		$fname = "../var/{$name}.{$type}";
+		if(!file_exists($fname)){
+			$f3->error(404);
+			exit();
+		}
+		switch($type){
+			case "json":
+				header('Content-type: application/json');
+			break;
+			case "csv":
+				header('Content-type: text/csv');
+			break;
+		}
+		readfile($fname);
+		exit();
+	}	
+);
+
+
 $f3->route('GET /data/autoopds.json',
 	function($f3) { 
 		header('Content-type: application/json');
@@ -72,6 +88,59 @@ $f3->route('GET /data/autoopds.json',
 		exit();
 	}	
 );
+
+
+$f3->set('ONERROR',function() use($f3) {
+ 	$f3=Base::instance();
+
+	$error = $f3->get('ERROR');
+	$error_title = constant('Base::HTTP_'.$error['code']);
+	
+	
+   	$f3->set('html_title', "{$error['code']} {$error_title}" );
+	$f3->set('content','content.html');
+	
+	$c[] = "<h2>{$error_title}</h2>";
+	
+	switch($error['code']){
+		case "404":
+			$c[] = "<p>The requested URL {$_SERVER['REDIRECT_URL']} was not found on this server.</p>";
+		break;
+	}
+	
+	if($f3->get('DEBUG')>0){
+		$c[] = "<hr/>";
+		$c[] = "<p>{$error['text']}</p>";
+		foreach ($error['trace'] as $frame) {
+			$line='';
+			if (isset($frame['file']) && 
+				(empty($frame['class']) || $frame['class']!='Magic') &&
+				(empty($frame['function']) || !preg_match('/^(?:(?:trigger|user)_error|__call|call_user_func)/',$frame['function']))
+				) {
+				
+				$addr=$f3->fixslashes($frame['file']).':'.$frame['line'];
+				if (isset($frame['class']))
+					$line.=$frame['class'].$frame['type'];
+				if (isset($frame['function'])) {
+					$line.=$frame['function'];
+					if (!preg_match('/{.+}/',$frame['function'])) {
+						$line.='(';
+						if (!empty($frame['args']))
+							$line.=$f3->csv($frame['args']);
+						$line.=')';
+					}
+				}
+				$str=$addr.' '.$line;
+				$c[] = '&bull; '.nl2br($f3->encode($str)).'<br />';
+			}
+		}
+	}
+	
+	$f3->set('html_content',join("",$c));
+
+	print Template::instance()->render( "page-template.html" );
+	exit();
+});
 
 
 $f3->run();
